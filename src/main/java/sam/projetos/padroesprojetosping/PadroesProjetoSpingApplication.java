99c587b514e240b0b0061900d4963fa8bfee68e1
Samuel Ribeiro <samuca3004@hotmail.com>
2023-03-13 14:30:53 -0300

    Primeiro Commit

---
+++ src/main/java/sam/projetos/padroesprojetosping/PadroesProjetoSpingApplication.java
@@ -0,0 +1,13 @@
+package sam.projetos.padroesprojetosping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesProjetoSpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjetoSpingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sam/projetos/padroesprojetosping/PadroesProjetoSpingApplicationTests.java
@@ -0,0 +1,13 @@
+package sam.projetos.padroesprojetosping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoSpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
