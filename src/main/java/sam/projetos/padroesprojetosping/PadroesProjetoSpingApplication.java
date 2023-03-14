8a290710e190c52ab84d596799125db7051ef753
Samuel Ribeiro <samuca3004@hotmail.com>
2023-03-14 11:02:40 -0300

    Resolvido erro Swagger-UI

--- src/main/java/sam/projetos/padroesprojetosping/PadroesProjetoSpingApplication.java
+++ src/main/java/sam/projetos/padroesprojetosping/PadroesProjetoSpingApplication.java
@@ -1,6 +1,6 @@
 package sam.projetos.padroesprojetosping;
 
-import io.swagger.v3.oas.models.annotations.OpenAPI30;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
